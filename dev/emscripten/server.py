21bbe5f6875464f5f871f4015caffeafb6403053
Heinz-Günter Hermanns <hgh.dev@t-online.de>
2026-01-17 16:40:29 +0000

    Added dev.

---
+++ dev/emscripten/server.py
@@ -0,0 +1,16 @@
+# https://til.bradyjoslin.com/wasm/local-server/
+import http.server
+import socketserver
+
+PORT = 8000
+
+Handler = http.server.SimpleHTTPRequestHandler
+Handler.extensions_map = {
+    '.html': 'text/html',
+    '.wasm': 'application/wasm',
+    '': 'application/octet-stream',
+}
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
